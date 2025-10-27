59963848c281362b50048591c8182cb94d97a017
Mayank Bisht <octpahadi@gmail.com>
2025-10-28 00:18:00 +0530

    added some featured work section 1.1.1

--- src/components/common/infinite-slider.tsx
+++ src/components/common/infinite-slider.tsx
@@ -95,7 +95,6 @@ export default function InfiniteSlider({ images, speed = 100, height = "300px", 
                             priority
                             src={src}
                             alt={`${alt} ${(i % images.length) + 1}`}
-                            loading="lazy"
                         />
                     </div>
                 ))}
